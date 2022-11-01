3d7c35e585462f34a0f878d7a41346d0bfe5c286
vytzab <vytautas.zabielskas@gmail.com>
2022-11-01 18:14:26 +0200

    Added HTTP Statuses to Expense Delete and Save endpoints

--- expenseTrackerApi/src/main/java/lt/viko/eif/vytzab/expenseTrackerApi/controller/ExpenseController.java
+++ expenseTrackerApi/src/main/java/lt/viko/eif/vytzab/expenseTrackerApi/controller/ExpenseController.java
@@ -3,12 +3,14 @@ package lt.viko.eif.vytzab.expenseTrackerApi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lt.viko.eif.vytzab.expenseTrackerApi.entity.Expense;
@@ -30,11 +32,13 @@ public class ExpenseController {
 		return expenseService.getExpenseById(id);
 	}
 
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
 	@DeleteMapping("/expenses/{id}")
 	public void deleteExpenseById(@PathVariable Long id) {
 		expenseService.deleteExpenseById(id);
 	}
 
+	@ResponseStatus(value = HttpStatus.CREATED)
 	@PostMapping("/expenses")
 	public Expense saveExpense(@RequestBody Expense expense) {
 		return expenseService.saveExpense(expense);
