e8ad3aaaf7df7cc896e4d2122bc8fe22c7046882
vytzab <vytautas.zabielskas@gmail.com>
2022-11-01 19:00:24 +0200

    Added MethodArgumentTypeMismatchException

--- expenseTrackerApi/src/main/java/lt/viko/eif/vytzab/expenseTrackerApi/exceptions/GlobalExceptionHandler.java
+++ expenseTrackerApi/src/main/java/lt/viko/eif/vytzab/expenseTrackerApi/exceptions/GlobalExceptionHandler.java
@@ -7,13 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import lt.viko.eif.vytzab.expenseTrackerApi.entity.ErrorObject;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<ErrorObject> handleExpenseNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+	public ResponseEntity<ErrorObject> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
 
 		ErrorObject errorObject = new ErrorObject();
 
@@ -22,5 +23,17 @@ public class GlobalExceptionHandler {
 		errorObject.setTimestamp(new Date());
 
 		return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	public ResponseEntity<ErrorObject> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex, WebRequest request) {
+
+		ErrorObject errorObject = new ErrorObject();
+
+		errorObject.setStatusCode(HttpStatus.BAD_REQUEST.value());
+		errorObject.setMessage(ex.getMessage());
+		errorObject.setTimestamp(new Date());
+
+		return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.BAD_REQUEST);
 	}
 }
