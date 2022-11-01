d1cc49a1caa0c78e97d57833d3eeefe67d353106
vytzab <vytautas.zabielskas@gmail.com>
2022-11-01 19:09:03 +0200

    Added GlobalException

--- expenseTrackerApi/src/main/java/lt/viko/eif/vytzab/expenseTrackerApi/exceptions/GlobalExceptionHandler.java
+++ expenseTrackerApi/src/main/java/lt/viko/eif/vytzab/expenseTrackerApi/exceptions/GlobalExceptionHandler.java
@@ -14,7 +14,8 @@ import lt.viko.eif.vytzab.expenseTrackerApi.entity.ErrorObject;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<ErrorObject> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+	public ResponseEntity<ErrorObject> handleResourceNotFoundException(ResourceNotFoundException ex,
+			WebRequest request) {
 
 		ErrorObject errorObject = new ErrorObject();
 
@@ -24,9 +25,10 @@ public class GlobalExceptionHandler {
 
 		return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.NOT_FOUND);
 	}
-	
+
 	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
-	public ResponseEntity<ErrorObject> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex, WebRequest request) {
+	public ResponseEntity<ErrorObject> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex,
+			WebRequest request) {
 
 		ErrorObject errorObject = new ErrorObject();
 
@@ -35,5 +37,17 @@ public class GlobalExceptionHandler {
 		errorObject.setTimestamp(new Date());
 
 		return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorObject> handleGeneralException(Exception ex, WebRequest request) {
+
+		ErrorObject errorObject = new ErrorObject();
+
+		errorObject.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+		errorObject.setMessage(ex.getMessage());
+		errorObject.setTimestamp(new Date());
+
+		return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
