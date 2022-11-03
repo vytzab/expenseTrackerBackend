a2341bc326700fa418e9f61b0881009af6db1695
vytzab <vytautas.zabielskas@gmail.com>
2022-11-03 14:57:45 +0200

    Added login endpoint

--- expenseTrackerApi/src/main/java/lt/viko/eif/vytzab/expenseTrackerApi/config/WebSecurityConfig.java
+++ expenseTrackerApi/src/main/java/lt/viko/eif/vytzab/expenseTrackerApi/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package lt.viko.eif.vytzab.expenseTrackerApi.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -32,5 +33,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 }
--- expenseTrackerApi/src/main/java/lt/viko/eif/vytzab/expenseTrackerApi/controller/AuthController.java
+++ expenseTrackerApi/src/main/java/lt/viko/eif/vytzab/expenseTrackerApi/controller/AuthController.java
@@ -5,10 +5,15 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import lt.viko.eif.vytzab.expenseTrackerApi.entity.AuthModel;
 import lt.viko.eif.vytzab.expenseTrackerApi.entity.User;
 import lt.viko.eif.vytzab.expenseTrackerApi.entity.UserModel;
 import lt.viko.eif.vytzab.expenseTrackerApi.service.IUserService;
@@ -16,11 +21,18 @@ import lt.viko.eif.vytzab.expenseTrackerApi.service.IUserService;
 @RestController
 public class AuthController {
 	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
 	private IUserService userService;
 
 	@PostMapping("/login")
-	public ResponseEntity<String> login() {
-		return new ResponseEntity<String>("User is logged in", HttpStatus.OK);
+	public ResponseEntity<HttpStatus> login(@RequestBody AuthModel authModel) {
+		Authentication authentication = authenticationManager
+				.authenticate(new UsernamePasswordAuthenticationToken(authModel.getEmail(), authModel.getPassword()));
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		return new ResponseEntity<HttpStatus>(HttpStatus.OK);
 	}
 
 	@PostMapping("/register")
