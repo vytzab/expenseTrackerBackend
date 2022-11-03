4f72bb93875c703c3524f4dd1160207b28e48de8
vytzab <vytautas.zabielskas@gmail.com>
2022-11-03 12:53:17 +0200

    Added in memory users

--- expenseTrackerApi/src/main/java/lt/viko/eif/vytzab/expenseTrackerApi/config/WebSecurityConfig.java
+++ expenseTrackerApi/src/main/java/lt/viko/eif/vytzab/expenseTrackerApi/config/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package lt.viko.eif.vytzab.expenseTrackerApi.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -17,6 +19,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated()
 			.and()
 			.httpBasic();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.inMemoryAuthentication()
+			.withUser("veeezeee").password("12345").authorities("admin")
+			.and()
+			.withUser("zeeeveee").password("12345").authorities("user")
+			.and()
+			.passwordEncoder(NoOpPasswordEncoder.getInstance());
 	}
 
 }
